954598c6f624e91ca775b9c41cc90b5010d14dbf
ImSurprisable <toastedflqme@gmail.com>
2025-03-01 01:31:22 -0800

    Added file uploading to Go backend
    POST route on "./upload"

--- music-player/backend/main.go
+++ music-player/backend/main.go
@@ -2,13 +2,48 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 )
 
 var db *sql.DB
+
+func uploadSong(c *gin.Context) {
+	// get the file
+	file, err := c.FormFile("file")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "No file attached!"})
+		return
+	}
+
+	uploadDir := "./uploads"
+
+	// save destination with inline string & filename
+	dst := fmt.Sprintf("%s/%s", uploadDir, file.Filename)
+	// check if uploads directory even exists, if not make it
+	_, err = os.Stat(uploadDir)
+	if os.IsNotExist(err) {
+		err := os.MkdirAll(uploadDir, os.ModePerm)
+		if err != nil {
+			log.Fatal("Failed to create uplaods directory: ", err)
+		}
+	}
+
+	// attempt to save the file locally in /backend/uploads
+	if err := c.SaveUploadedFile(file, dst); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save the file!"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": fmt.Sprintf("File %s uploaded successfully!", file.Filename),
+	})
+}
 
 func main() {
 	var err error // DANGER DANGER DANGER - USERNAME AND PASSWORD EXPOSED ON NEXT LINE:
@@ -19,10 +54,13 @@ func main() {
 	defer db.Close() // ensure the db connection is closed when the program ends
 
 	r := gin.Default() // set up gin
+
 	// add listener for GET requests on root directory saying that the backend is running
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "Backend is running!"})
 	})
+
+	r.POST("/upload", uploadSong)
 
 	r.Run(":8080")
 }
